ba6a3ab3b20a100b827554c8cd0495cac8975e21
Rafael <rafalapregagm2@gmail.com>
2026-05-15 11:36:05 -0300

    Linhas 10 a 25: criado o codigo main, e com ele foram criadas 3 contas exemplo com os dados e respectivas senhas armazenadas.

--- cp03.c
+++ cp03.c
@@ -6,3 +6,20 @@ typedef struct {
     char senha[20];
     float saldo;
 } Conta;
+
+int main() {
+
+    Conta contas[3];
+
+    contas[0].numeroConta = 1001;
+    strcpy(contas[0].senha, "senha1");
+    contas[0].saldo = 1500.50;
+
+    contas[1].numeroConta = 1002;
+    strcpy(contas[1].senha, "senha2");
+    contas[1].saldo = 3200.00;
+
+    contas[2].numeroConta = 1003;
+    strcpy(contas[2].senha, "senha3");
+    contas[2].saldo = 850.75;
+    }
